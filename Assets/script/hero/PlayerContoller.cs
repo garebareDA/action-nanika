5d96e170283d4f37275b2aeebd91c7bd91d54119
garebareDA <garedaiki@gmail.com>
2020-07-14 07:11:25 +0900

    add:ray

--- Assets/script/hero/PlayerContoller.cs
+++ Assets/script/hero/PlayerContoller.cs
@@ -19,13 +19,13 @@ public class PlayerContoller : MonoBehaviour
     private bool isJumpinig;
 
     public float gravity;
-
     public string gravityMode;
+
+    private float angleTmp;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
     }
 
     // Update is called once per frame
@@ -43,9 +43,6 @@ public class PlayerContoller : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, moveInput * -speed);
         }
-  
-        Vector3 gravityVector = gravietyDirection(gravityMode);
-        rb.AddForce(gravityVector);
     }
 
     void Update()
@@ -69,13 +66,17 @@ public class PlayerContoller : MonoBehaviour
             {
                 isJumpinig = false;
             }
-
         }
 
         if(Input.GetKeyUp(KeyCode.Space))
         {
             isJumpinig = false;
         }
+ 
+        Vector3 gravityVector = gravietyDirection(gravityMode);
+        rb.AddForce(gravityVector);
+
+        ray();
     }
 
     private Vector3 gravietyDirection(string mode)
@@ -84,22 +85,18 @@ public class PlayerContoller : MonoBehaviour
         {
             case "up":
                 Vector3 gravityVectorUp = new Vector3(0, -gravity, 0);
-                transform.eulerAngles = new Vector3(0, 0, 180f);
                 return gravityVectorUp;
 
             case "left":
                 Vector3 gravityVectorLeft = new Vector3 (gravity, 0, 0);
-                transform.eulerAngles = new Vector3(0, 0, -90f);
                 return gravityVectorLeft;
 
             case "right":
                 Vector3 gravityVectorRight = new Vector3(-gravity, 0, 0);
-                transform.eulerAngles = new Vector3(0, 0, 90f);
                 return gravityVectorRight;
 
             default:
                 Vector3 gravityVectorDown = new Vector3(0, gravity, 0);
-                transform.eulerAngles = Vector3.zero;
                 return gravityVectorDown;
         }
     }
@@ -126,14 +123,88 @@ public class PlayerContoller : MonoBehaviour
         }
     }
 
+    private Ray getGravityRay()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        if (gravityMode == "down")
+        {
+            ray = new Ray(transform.position + new Vector3(0, -0.5f, 0), -transform.up);
+        }
+        else if (gravityMode == "up")
+        {
+            ray = new Ray(transform.position + new Vector3(0, 0.5f, 0), -transform.up);
+        }
+        else if (gravityMode == "left")
+        {
+            ray = new Ray(transform.position + new Vector3(-0.5f, 0, 0), -transform.up);
+        }
+        else if (gravityMode == "right")
+        {
+            ray = new Ray(transform.position + new Vector3(0.5f, 0, 0), -transform.up);
+        }
+        return ray;
+    }
+
+    private void ray()
+    {
+        Ray ray = getGravityRay();
+        
+        RaycastHit2D[] hits = new RaycastHit2D[2];
+        int h = Physics2D.RaycastNonAlloc(ray.origin, ray.direction, hits);
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
+        if (h > 1)
+        {
+            Debug.Log(hits[1].normal);
+            if (hits[1].collider.tag == "ground")
+            {
+   
+                Quaternion q = Quaternion.FromToRotation(
+                        transform.up,
+                        hits[1].normal);
+                transform.rotation *= q;
+                Debug.Log(q);
+            }
+
+        }
+    }
+
     public void changeGravityMode(string mode)
     {
-        if(mode == "up" || mode == "down" || mode == "left" || mode =="right")
+        ray();
+        if (gravityMode != mode)
         {
-            gravityMode = mode;
-        }else
+            isJumpinig = false;
+            if (mode == "up" || mode == "down" || mode == "left" || mode =="right")
+            {
+                gravityMode = mode;
+            }
+            else
+            {
+                Debug.LogError("gravity change error");
+            }
+        }
+        else
         {
-            Debug.LogError("gravity change error");
+            return;
+        }
+
+        switch (mode)
+        {
+            case "up":
+                transform.eulerAngles = new Vector3(0, 0, 180f);
+                break;
+
+            case "down":
+                transform.eulerAngles = Vector3.zero;
+                break;
+
+            case "left":
+                transform.eulerAngles = new Vector3(0, 0, -90f);
+                break;
+
+            case "right":
+                transform.eulerAngles = new Vector3(0, 0, 90f);
+                break;
         }
     }
 }

ab073d9e59b477d59584c0bc3aaf270f43bfb007
garebareDA <garedaiki@gmail.com>
2020-07-27 02:23:42 +0900

    add:jump step

---
+++ Assets/script/jetEnemyRed/fire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fire : MonoBehaviour
+{
+    private Transform enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = transform.parent.gameObject.transform;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            enemy.SendMessage("fire");
+        }
+    }
+}
